7d1cee5b8426dd3b2e3f4dd9d876cdcd128cd1a3
Prahlad Reddy <prahladiosdev@gmail.com>
2024-02-23 12:07:44 -0500

    Initial Commit

---
+++ ShortPath/ShortPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortPathApp.swift
+//  ShortPath
+//
+//  Created by Sandhya on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ShortPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
